1470468bf4595f8277395c842e17b6e07d36d766
Oludare Olugbemi <ooo6hy@virginia.edu>
2022-02-13 22:47:21 -0500

    updated table

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PolicyService } from '../policy.service';
 
+
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
@@ -21,6 +22,6 @@ export class AdminPageComponent implements OnInit {
   headElements = ['First Name', 'Middle Name', 'Last Name', 'Email', 'Address One',
     'City', 'State', 'Country', 'Church Leadership', 'English Proficiency', 'Other Language', 'Phone Number', 'Gender', 'Ethnic Detail', 'Ethnic',
     'Occupation', 'Marital Status', 'Special Accomidations', 'Special Accomidations Explain', 
-    'Previous Conviction', 'Previous Convication Explain', 'Date of Birth', 'Address Two', 'Denomnination', 'Church Name', 'Church Size', 'Church Role', 'Country Two', 'Church Location', 'Attendance Length',
-    'Statement Purpose', 'InternetAccess', 'Refference Point', 'Anyone Studying Now', 'Date Submitted','Years of being a believer', 'Country Citizenship'];
+    'Previous Conviction', 'Previous Convication Explain', 'Date of Birth', 'Address Two', 'Denomnination', 'Church Name', 'Church Size', 'Church Role', 'Country Two', 'Statement Purpose', 'Attendance Length',
+    'Church Location', 'InternetAccess', 'Refference Point', 'Anyone Studying Now', 'Date Submitted','Years of being a believer', 'Country Citizenship'];
 }
